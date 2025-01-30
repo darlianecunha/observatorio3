8067759819516fbd0ee842359074ef81556b6b60
Darliane <140345464+darlianecunha@users.noreply.github.com>
2025-01-30 17:40:33 +0100

    Add files via upload

--- dashbord.py
+++ dashbord.py
@@ -22,7 +22,7 @@ st.markdown(
 )
 
 # Cabeçalho do Dashboard
-st.markdown("<h1 style='text-align: center; color: #003366;'>Dashboard de Mercadoria Movimentadas pelo Setor Aquaviário Brasileiro</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #003366;'>Dashboard de Mercadoria Movimentada pelo Setor Aquaviário por País e Produto</h1>", unsafe_allow_html=True)
 
 # Carregar os dados
 @st.cache_data
@@ -32,7 +32,8 @@ def load_data():
     df = df.rename(columns={
         'Ano': 'ano',
         'Nomenclatura Simplificada': 'tipo_produto',
-        'Total de Movimentação Portuária\nem toneladas (t)': 'movimentacao_total_t'
+        'Total de Movimentação Portuária\nem toneladas (t)': 'movimentacao_total_t',
+        'País Origem': 'pais'
     })
     df["ano"] = df["ano"].astype(int).astype(str)  # Garantir formato correto de ano
     return df
@@ -43,17 +44,25 @@ df = load_data()
 st.sidebar.header("Filtros")
 ano_selecionado = st.sidebar.selectbox("Selecione o Ano", sorted(df["ano"].unique()), index=0)
 tipo_produto_selecionado = st.sidebar.selectbox("Selecione o Tipo de Produto", ["Todos"] + list(df["tipo_produto"].unique()), index=0)
+pais_selecionado = st.sidebar.selectbox("Selecione o País", ["Todos"] + list(df["pais"].unique()), index=0)
 
 # Aplicar filtros
 df_filtered = df[df["ano"] == ano_selecionado]
 if tipo_produto_selecionado != "Todos":
     df_filtered = df_filtered[df_filtered["tipo_produto"] == tipo_produto_selecionado]
+if pais_selecionado != "Todos":
+    df_filtered = df_filtered[df_filtered["pais"] == pais_selecionado]
 
 # Agregar dados por ano
 df_summary = df_filtered.groupby("ano", as_index=False)["movimentacao_total_t"].sum()
 
+total_movimentacao = df[df["ano"] == ano_selecionado]["movimentacao_total_t"].sum()
+
+df_summary["percentual"] = (df_summary["movimentacao_total_t"] / total_movimentacao) * 100
+
 # Formatar os números para exibição
 df_summary["movimentacao_total_t"] = df_summary["movimentacao_total_t"].apply(lambda x: f"{x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+df_summary["percentual"] = df_summary["percentual"].apply(lambda x: f"{x:.2f}%")
 
 # Exibir tabela de dados agregados
 st.dataframe(df_summary, width=1000)
